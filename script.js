838497ca522dfed9760014c78615f3161bd4e175
Chintu jha <67228053+chintu72100@users.noreply.github.com>
2024-01-15 13:24:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function activateSearch() {
+    var searchComponent = document.getElementById('searchComponent');
+    var searchInput = document.getElementById('searchInput');
+
+    // Add 'active' class to the search component
+    searchComponent.classList.add('active');
+
+    // Focus on the input element
+    searchInput.focus();
+}
